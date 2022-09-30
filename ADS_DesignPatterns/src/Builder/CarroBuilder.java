24ee180405e933f8ee42129db559ee9e7a1e1cfe
Willyprp <89864941+Willyprp@users.noreply.github.com>
2022-09-29 23:10:48 -0300

    Add files via upload

---
+++ ADS_DesignPatterns/src/Builder/CarroBuilder.java
@@ -0,0 +1,37 @@
+package Builder;
+
+public class CarroBuilder {
+
+	private Carro carro;
+	
+	public CarroBuilder() {
+		carro = new Carro();
+	}
+	
+	public void buildPortas(Porta[] portas) {
+		carro.setPortas(portas);
+	}
+	
+	public void buildMotor(Motor m) {
+		carro.setMotor(m);;
+	}
+	
+	public void buildComputadorBordo(ComputadorBordo cb) {
+		carro.setComputadorBordo(cb);
+	}
+	
+	public void buildFreioABS(FreioABS f) {
+		carro.setFreioABS(f);
+	}
+
+	public Carro getCarro() {
+		return carro;
+	}
+
+	// devolve o carro
+	public void setCarro(Carro carro) {
+		this.carro = carro;
+	}
+	
+	
+}
