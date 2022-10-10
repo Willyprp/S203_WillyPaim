53686da9df76cfdf02f1f41f37e8b8633ba6fc54
Willyprp <89864941+Willyprp@users.noreply.github.com>
2022-10-10 20:09:27 -0300

    Aula dia 10_10

--- ADS_DesignPatterns/src/pizzaria/PizzaBuilderTest.java
+++ ADS_DesignPatterns/src/pizzaria/PizzaBuilderTest.java
@@ -22,4 +22,32 @@ public class PizzaBuilderTest {
 		
 	}
 
+	@Test
+	void deveInvalidarPizzaComTamanhoErrado() {
+		
+		try{
+			Pizza pizza = new PizzaBuilder()
+				.setSize(9)
+				.addCheese()
+				.addPepperoni()
+				.getPizza();
+				fail("Devia ter lançado exception")
+		}catch(IllegalStateException e){
+			e.printStackTrace();
+		}	
+	}
+
+	@Test
+	void deveInvalidarPizzaSemIngrediente() {
+		
+		try{
+			Pizza pizza = new PizzaBuilder()
+				.setSize(2)
+				.getPizza();
+				fail("Devia ter lançado exception")
+		}catch(IllegalStateException e){
+			e.printStackTrace();
+		}	
+	}
+
 }
